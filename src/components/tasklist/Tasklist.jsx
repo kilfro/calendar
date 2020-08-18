eaddecd7289a29615d5bdb178228ac0101dee105
Kirill Frolov <frolov.cyrill@gmail.com>
2020-08-18 13:41:39 +0300

    Tasks sorting

--- src/components/tasklist/Tasklist.jsx
+++ src/components/tasklist/Tasklist.jsx
@@ -5,9 +5,10 @@ import Task from './Task'
 const Tasklist = ({ tasks = [] }) =>
     <div className='task-list'>
         {tasks.length !== 0
-            ? tasks.map(task =>
-                <Task key={task.from.getTime()} task={task} />
-            )
+            ? tasks.sort((one, two) => one.from.getTime() - two.from.getTime())
+                .map(task =>
+                    <Task key={task.from.getTime()} task={task} />
+                )
             : <h2>Нет задач</h2>
         }
     </div>
