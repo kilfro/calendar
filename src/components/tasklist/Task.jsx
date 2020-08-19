8550ebb4722de388ac980c2046a0246180ec9971
Kirill Frolov <frolov.cyrill@gmail.com>
2020-08-19 12:59:41 +0300

    Remove button in task card

--- src/components/tasklist/Task.jsx
+++ src/components/tasklist/Task.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { X } from 'react-bootstrap-icons'
+import { removeTask } from '../../store/actions'
 import '@style/tasklist/task.less'
 
-const Task = ({ task }) => {
+const Task = ({ task, removeTask }) => {
     const { from, to, color, title, description } = task
 
     return (
         <div className={`task ${color}`}>
-            <h3>{`${from.getTimeString()} - ${to.getTimeString()} ${title}`}</h3>
+            <div className='top-row'>
+                <div className='time'>{from.getTimeString()} - {to.getTimeString()}</div>
+                <div className='title'>{title}</div>
+                <div className='buttons'>
+                    <X onClick={(() => removeTask(task.uid))} />
+                </div>
+            </div>
             <div>{description}</div>
         </div>
     )
 }
 
-export default Task
+const mapDispatchToProps = {
+    removeTask
+}
+
+export default connect(null, mapDispatchToProps)(Task)
