3671e9d3ab5bc7c65d21f12b4f727dac6b19711f
Kirill Frolov <frolov.cyrill@gmail.com>
2020-08-28 15:34:34 +0300

    Tests for reducer

--- __tests__/store/reducer.test.js
+++ __tests__/store/reducer.test.js
@@ -3,6 +3,10 @@ import * as actionCreator from '../../src/store/actions'
 import '../../src/prototypes'
 
 describe('Reducer', () => {
+  beforeEach(() => {
+    localStorage.clear()
+  })
+
   test('has to return default state', () => {
     const { now, selected, month, tasksMap } = reducer(undefined, {})
 
@@ -45,5 +49,120 @@ describe('Reducer', () => {
     const { now } = reducer(state, actionCreator.updateNow())
 
     expect(now.getString()).toEqual(new Date().getString())
+  })
+
+  test('has to add new task', () => {
+    const task = {
+      uid: 'UID',
+      title: 'New task',
+      from: new Date(2020, 4, 22, 10, 0),
+      to: new Date(2020, 4, 22, 10, 15),
+    }
+    const selected = new Date(2020, 4, 22)
+    const oldState = {
+      selected,
+      tasksMap: {},
+    }
+    const { tasksMap } = reducer(oldState, actionCreator.addTask(task))
+
+    expect(tasksMap.hasOwnProperty(selected.getString())).toBeTruthy()
+    expect(tasksMap[selected.getString()][0]).toEqual(task)
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      'tasksMap',
+      JSON.stringify(tasksMap)
+    )
+  })
+
+  test('has to add new task into existed date', () => {
+    const task = {
+      uid: 'UID',
+      title: 'New task',
+      from: new Date(2020, 4, 22, 10, 0),
+      to: new Date(2020, 4, 22, 10, 15),
+    }
+    const selected = new Date(2020, 4, 22)
+    const oldState = {
+      selected,
+      tasksMap: {
+        '2020-5-22': [
+          {
+            uid: 'Existed task',
+            title: 'Existed task',
+            from: new Date(2020, 4, 22, 9, 45),
+            to: new Date(2020, 4, 22, 10, 0),
+          },
+        ],
+      },
+    }
+    const { tasksMap } = reducer(oldState, actionCreator.addTask(task))
+
+    expect(tasksMap[selected.getString()].length).toEqual(2)
+    expect(tasksMap[selected.getString()].includes(task)).toBeTruthy()
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      'tasksMap',
+      JSON.stringify(tasksMap)
+    )
+  })
+
+  test('has to remove task correctly', () => {
+    const uidToRemove = 'REMOVE_IT'
+    const selected = new Date(2020, 4, 22)
+    const state = {
+      selected,
+      tasksMap: {
+        '2020-5-22': [
+          {
+            uid: '1',
+            title: 'One',
+          },
+          {
+            uid: uidToRemove,
+            title: 'Task to remove',
+          },
+        ],
+      },
+    }
+    const { tasksMap } = reducer(state, actionCreator.removeTask(uidToRemove))
+
+    expect(tasksMap[selected.getString()].length).toEqual(1)
+    expect(tasksMap[selected.getString()][0].uid).toEqual('1')
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      'tasksMap',
+      JSON.stringify(tasksMap)
+    )
+  })
+
+  test('has to edit task correctly', () => {
+    const uidToEdit = 'EDIT_IT'
+    const selected = new Date(2020, 4, 22)
+    const state = {
+      selected,
+      tasksMap: {
+        '2020-5-22': [
+          {
+            uid: '1',
+            title: 'One',
+          },
+          {
+            uid: uidToEdit,
+            title: 'Task to edit',
+          },
+        ],
+      },
+    }
+    const { tasksMap } = reducer(
+      state,
+      actionCreator.editTask({ uid: uidToEdit, title: 'Task was edited' })
+    )
+    const filteredTasks = tasksMap[selected.getString()].filter(
+      (task) => task.uid === uidToEdit
+    )
+
+    expect(filteredTasks.length).toEqual(1)
+    expect(filteredTasks[0].title).toEqual('Task was edited')
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      'tasksMap',
+      JSON.stringify(tasksMap)
+    )
   })
 })
